931b6fc9a87ad2386336d6fffca7287318d4fff2
tiagoomendess <tiago@mendes.com.pt>
2018-05-22 20:27:29 +0100

    asdasd

--- GameRefs.js
+++ GameRefs.js
@@ -4,11 +4,11 @@ var fs          = require('fs');
 
 var link = "http://www.afpbarcelos.pt/noticias?page=9";
 var nextPageLink = null;
-var min_date = new Date("2017-09-01");
+var min_date = new Date("2017-01-01");
 var max_date = new Date("2018-06-01");
 var exit = false;
 
-console.log("Searching between " + min_date + " and " + max_date);
+//console.log("Searching between " + min_date + " and " + max_date);
 
 readNews();
 
@@ -18,7 +18,7 @@ function readNews() {
 
     request(link, function(error, response, body) {
 
-        console.log("Requesting " + link);
+        //console.log("Requesting " + link);
     
         $ = cheerio.load(body);
         
@@ -43,8 +43,8 @@ function readNews() {
             }
             
             //Se for uma data mais recente que a que queremos, ignora
-            if (date.getTime() < max_date.getTime() && (title.match("ÁRBITROS NOMEADOS") || title.match("Árbitros Nomeados"))) {
-                console.log('Found Match!');
+            if (date.getTime() < max_date.getTime() && (title === "ÁRBITROS NOMEADOS" || title === "Árbitros Nomeados")) {
+                //console.log('Found Match!');
                 inspectNews(link);
             }
 
@@ -63,7 +63,7 @@ function inspectNews(link) {
 
     var $ = null;
     
-    console.log("Inspecting referees at " + link);
+    //console.log("Inspecting referees at " + link);
 
     request(link, function(error, response, body) {
 
@@ -98,6 +98,42 @@ function inspectNews(link) {
             linha = linha.replace(/\./g, '');
 
             //Get Competition - (\|([a-z\.]+\s)*[0-9]\ª\s[a-z\ã]+\|)|(\|[a-z\.\ç]+\|)|(\|[a-z\.\ç]+\s[a-z\.\ç]+\|)
+            match = linha.match(/(\|([a-z\.]+\s)*[0-9]\ª\s[a-z\ã]+\|)|(\|[a-z\.\ç]+\|)|(\|[a-z\.\ç]+\s[a-z\.\ç]+\|)/g);
+
+            if (match) {
+                competition = match.toString();
+                competition = competition.substring(1, competition.length - 1);
+            } else {
+                competition = "Desconhecida";
+            }
+
+            //Get Referee
+            match = linha.match(/\|[a-z\s\.\à\á\ã\â\è\é\ê\ì\í\ò\ó\õ\ô\ù\ú\ç]+\,/g);
+
+            referee = match.toString();
+            referee = referee.substring(1, referee.length - 1);
+
+            //Get date
+            /**
+             * 2017-12-21 - yyyy-mm-dd - [0-9]{4}(\-|\/)[0-9]{2}(\-|\/)[0-9]{2}
+             * 21-12-2017 - dd-mm-yyyy - [0-9]{2}(\-|\/)[0-9]{2}(\-|\/)[0-9]{4}
+             * 21/12/2017 - dd/mm/yyyy -  //
+             */
+
+            match = linha.match(/[0-9]{4}(\-|\/)[0-9]{2}(\-|\/)[0-9]{2}/g);
+            let splited;
+
+            if (match) {
+                splited = match.toString().split(/\-|\//g, 3);
+                date = new Date(splited[0] + "-" + splited[1] + "-" + splited[2]);
+            } else {
+                match = linha.match(/[0-9]{2}(\-|\/)[0-9]{2}(\-|\/)[0-9]{4}/g);
+                splited = match.toString().split(/\-|\//g, 3);
+                date = new Date(splited[2] + "-" + splited[1] + "-" + splited[0]);
+            }
+
+            console.log(date);
+            console.log(date.year + "-" + date.month + "-" + date.date);
 
         });
 
